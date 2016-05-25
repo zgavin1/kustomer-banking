f53c5a201beffa466e3d1dde6d39bfb0f5d52a23
Zach Gavin <gavinzach@gmail.com>
2016-05-25 11:48:02 -0400

    Add ledger reducer tests, remove ledger stuff from account reducer tests.

--- test/reducer.spec.js
+++ test/reducer.spec.js
@@ -29,17 +29,8 @@ describe('account reducer', () => {
       };
 
       expect(reducer(undefined, testAction))
-         .toEqual({
-            account: {balance: balance},
-            ledger: [
-               {
-                  amount: amount,
-                  balance: balance,
-                  id: id,
-                  date: date,
-                  type: C.DEPOSIT
-               }
-            ]
+         .toInclude({
+            account: {balance: balance}
          });
    });
 
@@ -56,17 +47,76 @@ describe('account reducer', () => {
       };
 
       expect(reducer(undefined, testWithdrawal))
+         .toInclude({
+            account: {balance: -5}
+         });
+   })
+});
+
+describe('ledger reducer', () => {
+   it('should return the initial state', () => {
+      expect(reducer(undefined, {}))
          .toEqual({
-            account: {balance: -5},
+            account: {balance: "0.00"},
+            ledger: []
+         });
+   });
+
+   it('should push new transactions into the ledger', () => {
+      const amount = Math.floor(Math.random() * 100),
+         balance = Math.floor(Math.random() * 100),
+         id = Math.floor(Math.random() * 10),
+         date = new Date(Math.floor(Math.random() * Date.now()));
+
+      const testAction = {
+         type: C.DEPOSIT,
+         transaction: {
+            amount: amount,
+            balance: balance,
+            id: id,
+            date: date,
+            type: C.DEPOSIT
+         }
+      };
+
+      expect(reducer(undefined, testAction))
+         .toInclude({
             ledger: [
                {
-                  amount: 5,
-                  balance: -5,
-                  id: 10,
-                  date: "Monday",
-                  type: C.WITHDRAWAL
+                  amount: amount,
+                  balance: balance,
+                  id: id,
+                  date: date,
+                  type: C.DEPOSIT
                }
             ]
          });
    })
-});
+
+   it('should handle multiple transactions', () => {
+      const amount = Math.floor(Math.random() * 100),
+         balance = Math.floor(Math.random() * 100),
+         id = Math.floor(Math.random() * 10),
+         date = new Date(Math.floor(Math.random() * Date.now()));
+
+      const testAction = {
+         type: C.WITHDRAWAL,
+         transaction: {
+            amount: amount,
+            balance: balance,
+            id: id,
+            date: date,
+            type: C.WITHDRAWAL
+         }
+      };
+
+      let testState;
+      for (var i = 0; i < 5; i++) {
+         testState = reducer(testState, testAction)
+      }
+
+      expect(testLedger.ledger.length)
+         .toEqual(5);
+   })
+
+})
